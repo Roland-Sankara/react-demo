e3643da1383311b67a6b756af980c7f3e8a556e4
Roland-Sankara <roland1sankara@gmail.com>
2020-06-30 14:52:01 +0300

    Updates

---
+++ precache-manifest.c45949a4dad89bd4bb3211a98ac2dc65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e301df912915ca5cb8a629552500f1fa",
+    "url": "/react-demo/index.html"
+  },
+  {
+    "revision": "f5fabfd4b631b49c7297",
+    "url": "/react-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f9e9f67bad663953fe5d",
+    "url": "/react-demo/static/js/2.d39f5f4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-demo/static/js/2.d39f5f4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5fabfd4b631b49c7297",
+    "url": "/react-demo/static/js/main.1164cc68.chunk.js"
+  },
+  {
+    "revision": "e91f5e23d011ab615da4",
+    "url": "/react-demo/static/js/runtime-main.5f8d9a6d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
